60f08da6e17e7196f1e9ea46e6060b4ec544a2c6
ctdlog <qpflapffhs76@gmail.com>
2023-05-17 16:06:14 +0000

    ✨ add meta tag for thumbnail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -15,6 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <Head>
+        <meta name='thumbnail' content='../../public/memorip_logo.png' />
+        <meta property='og:url' content='https://memorip.vercel.app' />
+        <meta property='og:image' content='../../public/memorip_logo.png' />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
