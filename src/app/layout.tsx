8e805d3d6f592c4c7cedd86507475c4f444f6b25
ctdlog <qpflapffhs76@gmail.com>
2023-05-17 16:17:24 +0000

    ✨ add meta thumbnail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,15 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <Head>
-        <meta name='thumbnail' content='../../public/memorip_logo.png' />
+        <meta
+          name='thumbnail'
+          content='https://user-images.githubusercontent.com/73215539/239014594-c8083529-075f-44f5-ac83-af80eeeba32f.png'
+        />
         <meta property='og:url' content='https://memorip.vercel.app' />
-        <meta property='og:image' content='../../public/memorip_logo.png' />
+        <meta
+          property='og:image'
+          content='https://user-images.githubusercontent.com/73215539/239014594-c8083529-075f-44f5-ac83-af80eeeba32f.png'
+        />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
