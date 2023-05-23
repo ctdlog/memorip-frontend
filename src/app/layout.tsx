ccf9e701c9252a387580108bf3a6f56c5ab6af7c
ctdlog <qpflapffhs76@gmail.com>
2023-05-23 14:37:18 +0000

    🐛 cls 빌드 에러 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,29 @@
-import Head from 'next/head';
-import './globals.css';
-import { Poppins, Noto_Sans_KR } from 'next/font/google';
+import Head from 'next/head'
+import './globals.css'
+import { Poppins, Noto_Sans_KR } from 'next/font/google'
 
 const notoSansKr = Noto_Sans_KR({
   subsets: ['latin'],
   weight: ['400', '700'],
   variable: '--font-notoSansKr',
-});
+})
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['400', '700'],
   variable: '--font-poppins',
-});
+})
 
-export const cls = (...classnames: string[]) => {
-  return classnames.join(' ');
-};
+const cls = (...classnames: string[]) => {
+  return classnames.join(' ')
+}
 
 export const metadata = {
   title: 'Memorip - 여행을 기록해봐요!',
   description: '여행을 기록해봐요!',
-};
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
       <head>
@@ -41,9 +37,7 @@ export default function RootLayout({
           content='https://user-images.githubusercontent.com/73215539/239014594-c8083529-075f-44f5-ac83-af80eeeba32f.png'
         />
       </head>
-      <body className={cls(notoSansKr.className, poppins.className)}>
-        {children}
-      </body>
+      <body className={cls(notoSansKr.className, poppins.className)}>{children}</body>
     </html>
-  );
+  )
 }
