5c17ea671bb8c944f66c85171d68715ff761f6ac
ctdlog <qpflapffhs76@gmail.com>
2023-05-24 15:40:44 +0000

    💄 font-weight 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className='flex flex-col justify-between items-center h-screen py-12 px-8'>
       <div>
         <Image src='/images/logo.png' width={200} height={200} alt='logo' />
-        <p className='text-center text-xl'>
+        <p className='text-center text-2xl font-bold'>
           지금 Memorip에서 <br />
           여행 계획을 짜보세요!
         </p>
@@ -15,13 +15,13 @@ export default function Home() {
       <div className='flex flex-col gap-4 w-full '>
         <Link
           href={ROUTE.SIGN_UP}
-          className='py-4 bg-blue-500 w-full rounded-lg text-white text-xl text-bold text-center'
+          className='py-4 bg-blue-500 w-full rounded-lg text-white text-xl font-bold text-center'
         >
           이메일로 회원가입
         </Link>
         <Link
           href={ROUTE.SIGN_IN}
-          className='py-4 w-full rounded-lg text-black border border-zinc-500 text-xl text-bold text-center'
+          className='py-4 w-full rounded-lg text-black border border-zinc-500 text-xl font-bold text-center'
         >
           로그인
         </Link>
